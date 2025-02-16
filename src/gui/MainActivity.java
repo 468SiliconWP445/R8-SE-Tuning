820149c45e3317ac7a5701154466b8466fac0c33
468SiliconWP445 <172748012+468SiliconWP445@users.noreply.github.com>
2025-02-16 22:30:41 +0000

    properly visible on launch

--- src/gui/MainActivity.java
+++ src/gui/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/a604958692/r8setuning/view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 400, 500);
+        Scene scene = new Scene(fxmlLoader.load(), 800, 800);
 
         primaryStage.setTitle("R8 SE Tuning");
         primaryStage.setScene(scene);
