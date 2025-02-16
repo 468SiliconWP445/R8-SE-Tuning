701fb735fa0b82c296dfe19b65227984a9390782
468SiliconWP445 <172748012+468SiliconWP445@users.noreply.github.com>
2025-02-16 22:18:20 +0000

    Create MainActivity.java

---
+++ src/gui/MainActivity.java
@@ -0,0 +1,22 @@
+package com.a604958692.r8setuning;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainActivity extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/a604958692/r8setuning/view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 400, 500);
+
+        primaryStage.setTitle("R8 SE Tuning");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
