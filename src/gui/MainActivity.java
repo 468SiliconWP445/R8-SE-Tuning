860e4b0ce7ff67009b9b55878826dd14f0d53f69
468SiliconWP445 <172748012+468SiliconWP445@users.noreply.github.com>
2025-02-21 20:55:09 +0000

    Update MainActivity.java

--- src/gui/MainActivity.java
+++ src/gui/MainActivity.java
@@ -8,7 +8,7 @@ import javafx.stage.Stage;
 public class MainActivity extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/a604958692/r8setuning/view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/a604958692/r8setuning/view.fxml")); //root
         Scene scene = new Scene(fxmlLoader.load(), 800, 800);
 
         primaryStage.setTitle("R8 SE Tuning");
